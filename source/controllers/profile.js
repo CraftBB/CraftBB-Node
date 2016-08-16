19a3161e9c002dee71adb4a8a1ba1620aa959dd3
Sean <me@sseann.com>
2016-08-16 04:19:16 +0000

    Updated error handling for user profile

--- source/controllers/profile.js
+++ source/controllers/profile.js
@@ -6,11 +6,9 @@ const User = require('../models/user')
 router.get('/:uuid', function  (req, res) {
   User.findOne({uuid: req.params.uuid}, function  (err, user) {
     if (err) throw err
-    var exists = (user ? true : false)
     res.render('profile/index', {
       user: user,
-      page: req.params.uuid,
-      exists: exists
+      page: req.params.uuid
     })
   })
 })
